71b18a89313b3c01f3bf0c912911144893d987dc
RAHEESL <raheeslaleelakath@gmail.com>
2023-07-05 16:37:00 +0300

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,25 +6,30 @@ import image from '../assets/OIP.jpg'
 const Home = () => {
     return (
         <>
-            <div class="nav">
-                <div class="image">
-                    <img src={image} />
-                </div>
-                <div className="aa">Spotify</div>
-                <div class="a">Premium</div>
-                <div class="b">Help</div>
-                <div class="c">Download</div>
-                <div class="d">|</div>
-                <Link to={"/register"} >
-                    <div class="e">Sign up</div>
-                </Link>
-                <Link to={"/login"} >
-                    <div class="f">Login</div>
-                </Link>
-            </div>
-        </>
-
-    )
-}
-
+        <div className='contai'>
+          <div className='nav'>
+          <div className='image'>
+            <img src={image} alt="" />
+          </div>
+          <p>Spotify</p>
+          <div className='a'>Premium</div>
+          <div className='b'>Help</div>
+          <div className='c'>Download</div>
+          <div className="k">|</div>
+          <Link to={"/Register"}>
+            <div className='d'>Sign Up</div>
+          </Link>
+          <Link to={"/login"}>
+            <div className='e'>Log In</div>
+          </Link>
+        </div><div className="container">
+            <div className='sen1'>Music for Eveyone</div>
+            <div className='sen2'>Millions of songs.No credit card needed</div>
+            <Link to={"/login"}>
+            <button className='but'>GET SPOTIFY FREE</button>
+            </Link>
+          </div>
+          </div></>
+      )
+    }
 export default Home
